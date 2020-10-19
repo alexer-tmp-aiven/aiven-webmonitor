da0e28987d4e33da4072e4434cf93ea1c9050e9e
Aleksi Torhamo <aleksi@torhamo.net>
2020-10-19 06:43:23 +0300

    Move everything to config and pass it properly

--- consumer.py
+++ consumer.py
@@ -3,28 +3,25 @@ import psycopg2
 import psycopg2.extras
 import json
 import time
-from config import KAFKA_OPTS, PG_DSN
-
-BATCH_SIZE = 10
 
 def json_deserialize(value):
 	return json.loads(value.decode('utf-8'))
 
-def main():
-	conn = psycopg2.connect(PG_DSN)
+def main(config):
+	conn = psycopg2.connect(config.PG_DSN)
 	consumer = kafka.KafkaConsumer(
 		'webmonitor',
 		group_id='webmonitor',
 		auto_offset_reset='earliest',
 		enable_auto_commit=False,
 		value_deserializer=json_deserialize,
-		**KAFKA_OPTS
+		**config.KAFKA_OPTS
 	)
 
 	batch = []
 	cur = conn.cursor()
 	for msg in consumer:
-		if len(batch) >= BATCH_SIZE:
+		if len(batch) >= config.BATCH_SIZE:
 			psycopg2.extras.execute_batch(cur, 'SELECT insert_event(%s, %s, %s, %s, %s, %s, %s)', batch)
 			batch = []
 			conn.commit()
@@ -36,5 +33,7 @@ def main():
 		batch.append((timestamp, data['url'], ok, data.get('http_status'), data.get('response_time'), data.get('content_ok'), data.get('error')))
 
 if __name__ == '__main__':
-	main()
+	import config
+
+	main(config)
 
--- producer.py
+++ producer.py
@@ -4,9 +4,7 @@ import aiokafka
 import time
 import json
 import re
-from config import KAFKA_OPTS, URLS
 
-TIMEOUT = aiohttp.ClientTimeout(total=10)
 HEADERS = {
 	'User-Agent': 'webmonitor/0.1',
 }
@@ -14,20 +12,20 @@ HEADERS = {
 def json_serialize(value):
 	return json.dumps(value).encode('utf-8')
 
-async def fetch_url(url):
+async def fetch_url(config, url):
 	# Create a new session for each request, so that all timings include connection establishment
-	async with aiohttp.ClientSession(headers=HEADERS, timeout=TIMEOUT) as session:
+	async with aiohttp.ClientSession(headers=HEADERS, timeout=config.HTTP_TIMEOUT) as session:
 		start_time = time.time()
 		async with session.get(url) as resp:
 			await resp.read()
 			resp_time = time.time() - start_time
 			return resp_time, resp
 
-async def check_url(producer, url, pattern=None):
+async def check_url(config, producer, url, pattern=None):
 	timestamp_ms = time.time() * 1000
 
 	try:
-		resp_time, resp = await fetch_url(url)
+		resp_time, resp = await fetch_url(config, url)
 	except aiohttp.ClientConnectorError as e:
 		result = {'url': url, 'error': repr(e.os_error)}
 	except aiohttp.ClientError as e:
@@ -53,21 +51,23 @@ async def check_url(producer, url, pattern=None):
 		timestamp_ms=timestamp_ms
 	)
 
-async def main(urls):
+async def main(config):
 	producer = aiokafka.AIOKafkaProducer(
 		value_serializer=json_serialize,
-		**KAFKA_OPTS
+		**config.KAFKA_OPTS
 	)
 
 	await producer.start()
 	try:
 		while True:
-			for url, pattern in urls:
-				asyncio.create_task(check_url(producer, url, pattern))
-			await asyncio.sleep(1)
+			for url, pattern in config.URLS:
+				asyncio.create_task(check_url(config, producer, url, pattern))
+			await asyncio.sleep(config.CHECK_FREQUENCY)
 	finally:
 		await producer.stop()
 
 if __name__ == '__main__':
-	asyncio.run(main(URLS))
+	import config
+
+	asyncio.run(main(config))
 
